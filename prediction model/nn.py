f4af0385f5713cbdf5ea06c1e4183748d20e6583
qzodiumchloride <57839464+qzodiumchloride@users.noreply.github.com>
2020-03-29 21:20:17 -0400

    (hopefully) final

--- prediction model/nn.py
+++ prediction model/nn.py
@@ -17,20 +17,30 @@ weeb_dir = 'prediction model/moeimouto-faces/'
                                   Y_test, label_test) = hf.test_train_split()
 
 
+FILTER_SIZE = 3
+NUM_FILTERS = 32
+INPUT_SIZE = 32
+MAXPOOL_SIZE = 2
+BATCH_SIZE = 16
+EPOCHS = 10
+
 model = Sequential()
-model.add(Conv2D(32, (3, 3),
-                 input_shape=(32, 32, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(NUM_FILTERS, (FILTER_SIZE, FILTER_SIZE),
+                 input_shape=(INPUT_SIZE, INPUT_SIZE, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(MAXPOOL_SIZE, MAXPOOL_SIZE)))
+model.add(Conv2D(NUM_FILTERS*2, (FILTER_SIZE, FILTER_SIZE), activation='relu'))
+model.add(MaxPooling2D(pool_size=(MAXPOOL_SIZE, MAXPOOL_SIZE)))
 model.add(Flatten())
 model.add(Dense(units=1000, activation='relu'))
 model.add(Dense(units=173, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-hist = model.fit(X_train, Y_train,
-                 156, 10, validation_split=0.3)
+try:
+    hist = model.fit(X_train, Y_train,
+                     BATCH_SIZE, EPOCHS, validation_split=0.3)
+except:
+    pass
 
 model.evaluate(X_test, Y_test)
 
